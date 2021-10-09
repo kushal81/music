a7ccce546fd250d928a8b417aa7d8764c7e78c7d
kushal81 <83442979+kushal81@users.noreply.github.com>
2021-10-09 15:08:48 +0530

    Update functions.js

--- handlers/functions.js
+++ handlers/functions.js
@@ -780,7 +780,7 @@ function onCoolDown(message, command) {
    */
   function change_status(client) {
     try {
-      client.user.setActivity(`${config.prefix}help | ${client.guilds.cache.size} Guilds | ${Math.ceil(client.users.cache.size/1000)}k Members`, {
+      client.user.setActivity(`${config.prefix}help | ${client.guilds.cache.size} Guilds | by Kushal Agarwal`, {
         type: "PLAYING",
       });
     } catch (e) {
